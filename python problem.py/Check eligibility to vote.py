d576410aa1445774d587103c8035ea91c76df145
Shaik.Mehak meeraj <143590675+mehalkshaik501@users.noreply.github.com>
2025-07-27 18:44:20 +0530

    Add files via upload

---
+++ python problem.py/Check eligibility to vote.py
@@ -0,0 +1,9 @@
+marks=int(input("Enter your marks"))
+if(marks>=90):
+    print("Grade: A")
+elif(marks>=80 and marks<90):
+    print("Grade: B")
+elif(marks>=70 and marks<80):
+    print("Grade: c")
+else:
+    print("Grade: D")
---
+++ python problem.py/operators.py
@@ -0,0 +1,41 @@
+#addition
+# Addition Example
+a = 10
+b = 5
+result = a + b
+print("Addition:", result)
+#substraction
+# Subtraction Example
+a = 10
+b = 5
+result = a - b
+print("Subtraction:", result)
+#mutiplication
+# Multiplication Example
+a = 10
+b = 5
+result = a * b
+print("Multiplication:", result)
+#division(/)=return a float
+# Division Example
+a = 10
+b = 5
+result = a / b
+print("Division:", result)
+#floor division(//)-return the integer part only
+# Floor Division Example
+a = 10
+b = 3
+result = a // b
+print("Floor Division:", result)
+#modulus(%)-gives the remainder
+# Modulus Example
+a = 10
+b = 3
+result = a % b
+print("Modulus:", result)
+# Exponentiation Example
+a = 2
+b = 3
+result = a ** b
+print("Exponentiation:", result)
