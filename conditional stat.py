922ce859f85dd896dd2c8e79aa1e013b6c5d1444
Shaik.Mehak meeraj <143590675+mehalkshaik501@users.noreply.github.com>
2025-07-27 18:41:21 +0530

    Add files via upload

---
+++ conditional stat.py
@@ -0,0 +1,17 @@
+#conditional Statements
+# single line if Ternary Operator
+# syntax <var>=<var1>if <condition> else <val2>
+food=input("food:")
+eat="yes" if food=="cake" else "no"
+print(eat)
+# <stt1>if condition> else <stt2>
+food=input("food:")
+print("sweet") if food=="cake" or food =="jalebi" else print("not sweet")
+#clever if /Ternary operator
+#<avr>=(false_val,true_val)[<condition>]
+age=int(input("age:"))dx
+vote=("yes", "no")[age<18]
+sal=float(input("salary:"))
+sal=float(input("salary:"))        
+tax=sal*(0.1, 0.2) [sal>50000]
+print(tax)
