c7c97cc1bf8c7e4fa079bd9acf68d1ebaa5bcf55
Shaik.Mehak meeraj <143590675+mehalkshaik501@users.noreply.github.com>
2025-07-28 20:37:30 +0530

    Add files via upload

---
+++ values.py
@@ -0,0 +1,9 @@
+#fig out to way to store 9&9.0 as separate values in the set.
+values={9,9.25,8,8.0}
+print(values)
+#take help bulit - in data type
+values={
+    ("float",9.0),
+     ("int",9)
+     }
+print(values)
